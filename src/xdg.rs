3d3597fd0cd39d89877ba479962c00c88de93f50
Elvyria <elvyria@tutanota.com>
2024-01-29 00:03:05 +0300

    Replaced macro with function, compiler will figure out inlining

--- src/xdg.rs
+++ src/xdg.rs
@@ -1,23 +1,21 @@
 use std::{env, path::PathBuf};
 
-macro_rules! xdg {
-    ($env:expr, $dir:expr) => {
-        env::var_os($env)
-            .map(PathBuf::from)
-            .or_else(|| {
-                env::var_os("HOME")
-                    .map(PathBuf::from)
-                    .map(|mut p| { p.push($dir); p })
-            })
-        .expect(concat!("couldn't find ", $env, " directory"))
-    };
+fn env_or_default(env: &str, fallback: &str) -> PathBuf {
+    env::var_os(env)
+        .map(PathBuf::from)
+        .or_else(|| {
+            env::var_os("HOME")
+                .map(PathBuf::from)
+                .map(|mut p| { p.push(fallback); p })
+        })
+    .unwrap_or_else(|| panic!("couldn't find the {env} directory"))
 }
 
 pub fn config_dir() -> PathBuf {
-    xdg!("XDG_CONFIG_HOME", ".config")
+    env_or_default("XDG_CONFIG_HOME", ".config")
 }
 
 #[cfg(feature = "Sass")]
 pub fn cache_dir() -> PathBuf {
-    xdg!("XDG_CACHE_HOME", ".cache")
+    env_or_default("XDG_CACHE_HOME", ".cache")
 }
