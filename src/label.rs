2ef8c002582cc421041108ce56b78be8a77308e9
Elvyria <elvyria@tutanota.com>
2024-03-21 06:04:50 +0300

    Replaced `static` label notation with `const`

--- src/label.rs
+++ src/label.rs
@@ -1,18 +1,18 @@
 use color_print::cstr;
 
-pub static ERROR: &str = cstr!("<s><r>Error</></>");
+pub const ERROR: &str = cstr!("<s><r>Error</></>");
 
 #[allow(dead_code)]
-pub static WARNING: &str = cstr!("<r>Warning</>");
+pub const WARNING: &str = cstr!("<r>Warning</>");
 
 #[cfg(not(feature = "Wayland"))]
-pub static WAYLAND: &str = cstr!("<g>Wayland</>");
+pub const WAYLAND: &str = cstr!("<g>Wayland</>");
 
 #[cfg(not(feature = "X11"))]
-pub static X11: &str = cstr!("<g>X11</>");
+pub const X11: &str = cstr!("<g>X11</>");
 
 #[cfg(not(feature = "Sass"))]
-pub static SASS: &str = cstr!("<g>Sass</>");
+pub const SASS: &str = cstr!("<g>Sass</>");
 
 #[cfg(feature = "Wayland")]
-pub static LAYER_SHELL_PROTOCOL: &str = cstr!("<g>zwlr_layer_shell_v1</>");
+pub const LAYER_SHELL_PROTOCOL: &str = cstr!("<g>zwlr_layer_shell_v1</>");
